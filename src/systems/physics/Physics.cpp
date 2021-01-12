52c25ca4fcf5cdb8eb229cc4b439c279c9fdaeff
Miguel Angel <mlopezreynaldo@gmail.com>
2021-01-12 16:08:36 +0100

    Set up first systems

---
+++ src/systems/physics/Physics.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Miguel Angel on 12/1/21.
+//
+
+#include "Physics.h"
---
+++ src/systems/physics/Physics.h
@@ -0,0 +1,14 @@
+//
+// Created by Miguel Angel on 12/1/21.
+//
+
+#ifndef SIMENGINE_PHYSICS_H
+#define SIMENGINE_PHYSICS_H
+
+
+class Physics {
+
+};
+
+
+#endif //SIMENGINE_PHYSICS_H
