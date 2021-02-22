faf6540908a968e1566bf0b4b9141427ad3c5133
Miguel Angel <mlopezreynaldo@gmail.com>
2021-02-22 17:45:02 +0100

    .

---
+++ src/messaging/bus/MessageBus.h
@@ -0,0 +1,14 @@
+//
+// Created by Miguel Angel on 12/1/21.
+//
+
+#ifndef SIMENGINE_MESSAGEBUS_H
+#define SIMENGINE_MESSAGEBUS_H
+
+
+class MessageBus {
+
+};
+
+
+#endif //SIMENGINE_MESSAGEBUS_H
---
+++ src/messaging/message/Msg.h
@@ -0,0 +1,15 @@
+//
+// Created by Miguel Angel on 12/1/21.
+//
+
+#ifndef SIMENGINE_MSG_H
+#define SIMENGINE_MSG_H
+
+
+class Msg {
+public:
+    char type;
+};
+
+
+#endif //SIMENGINE_MSG_H
